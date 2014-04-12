9aeba6706b6c3fe81c187a645b8408695b6deaac
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2014-04-11 22:40:14 -0300

    adds test for general registers

--- cpu.py
+++ cpu.py
@@ -10,12 +10,12 @@ class Cpu:
         self.pc = Cpu.RAM_START
         self.sp = Cpu.STACK_START
         self.r  = [None] * (0xF + 1)
-        self.flag = 0x0000
+        self.flag = 0b00000000
         self.memory = [None] * (0xFFFF + 1)
 
     def write(self, address, value):
         self.memory[address]   = value & 0xFF
-        self.memory[address+1] = value >> 8
+        self.memory[address + 1] = value >> 8
 
     def read(self, address):
         return (self.memory[address + 1] << 8) | self.memory[address]
--- test_cpu.py
+++ test_cpu.py
@@ -12,3 +12,26 @@ def test_little_endianess():
 
     hex(chip16.read(0x000)).should.eql("0xffaa")
 
+def test_program_counter():
+    chip16 = cpu.Cpu()
+
+    chip16.pc = 0xCAFE
+
+    chip16.pc.should.eql(0xCAFE)
+
+def test_stack_pointer():
+    chip16 = cpu.Cpu()
+
+    chip16.sp = 0xCAFE
+
+    chip16.sp.should.eql(0xCAFE)
+
+def test_general_registers():
+    chip16 = cpu.Cpu()
+
+    for r in range(0x0,0xF):
+        chip16.r[r] = 0x00FA
+
+    chip16.r[0x0].should.eql(0x00FA)
+    chip16.r[0xF-1].should.eql(0x00FA)
+
