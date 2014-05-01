eb6c9140944601d61e35e56aba991c85694d2073
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2014-05-01 17:30:53 -0300

    adds all color to palette

--- gpu.py
+++ gpu.py
@@ -1,13 +1,33 @@
 import logging
 
 class Gpu:
-
     def __init__(self):
         self.bg = 0b0000
         self.spritew = 0x00
         self.spriteh = 0x00
         self.hflip = False
         self.vflip = False
+        self.__init_palette()
+
+    def __init_palette(self):
+        # rgb fitable for opengl
+        self.palette = {}
+        self.palette[0x0] = {'r': 0, 'g': 0, 'b': 0}
+        self.palette[0x1] = {'r': 0, 'g': 0, 'b': 0}
+        self.palette[0x2] = {'r': 136.0/255.0, 'g': 136.0/255.0, 'b': 136.0/255.0}
+        self.palette[0x3] = {'r': 191.0/255.0, 'g': 57.0/255.0, 'b': 50.0/255.0}
+        self.palette[0x4] = {'r': 222.0/255.0, 'g': 122.0/255.0, 'b': 174.0/255.0}
+        self.palette[0x5] = {'r': 76.0/255.0, 'g': 61.0/255.0, 'b': 33.0/255.0}
+        self.palette[0x6] = {'r': 144.0/255.0, 'g': 95.0/255.0, 'b': 37.0/255.0}
+        self.palette[0x7] = {'r': 228.0/255.0, 'g': 148.0/255.0, 'b': 82.0/255.0}
+        self.palette[0x8] = {'r': 234.0/255.0, 'g': 217.0/255.0, 'b': 121.0/255.0}
+        self.palette[0x9] = {'r': 83.0/255.0, 'g': 122.0/255.0, 'b': 59.0/255.0}
+        self.palette[0xA] = {'r': 171.0/255.0, 'g': 213.0/255.0, 'b': 74.0/255.0}
+        self.palette[0xB] = {'r': 37.0/255.0, 'g': 46.0/255.0, 'b': 56.0/255.0}
+        self.palette[0xC] = {'r': 0.0, 'g': 70.0/255.0, 'b': 127.0/255.0}
+        self.palette[0xD] = {'r': 104.0/255.0, 'g': 171.0/255.0, 'b': 204.0/255.0}
+        self.palette[0xE] = {'r': 188.0/255.0, 'g': 222.0/255.0, 'b': 228.0/255.0}
+        self.palette[0xF] = {'r': 1, 'g': 1, 'b': 1}
 
     def clear_fg(self):
         pass
