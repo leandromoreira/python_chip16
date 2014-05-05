7cc140352fcfeff6ec0a3684db23cd72f6640535
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2014-05-04 22:32:24 -0300

    tests all mod operations

--- test_cpu.py
+++ test_cpu.py
@@ -1212,3 +1212,59 @@ def test_DIV_rz():
     chip16.r[0x4].should.be.eql(0x1)
     chip16.flag_carry.should.be.eql(0x1)
     chip16.flag_negative.should.be.eql(0x0)
+
+def test_MODI():
+    #Set RX to RX MOD HHLL
+    chip16 = cpu.Cpu()
+
+    initial_address = 0x0000
+    chip16.pc = initial_address
+
+    chip16.write(initial_address, 0xA3) #op code
+    chip16.write(initial_address + 1, 0b00100001) #y,x
+    chip16.write(initial_address + 2, 0x3) #ll
+    chip16.write(initial_address + 3, 0x0) #hh
+
+    chip16.r[0x1] = 0x4
+
+    chip16.step()
+
+    chip16.r[0x1].should.be.eql(0x4 % 0x3)
+
+def test_MOD_rx_ry():
+    #Set RX to RX MOD RY
+    chip16 = cpu.Cpu()
+
+    initial_address = 0x0000
+    chip16.pc = initial_address
+
+    chip16.write(initial_address, 0xA4) #op code
+    chip16.write(initial_address + 1, 0b00100001) #y,x
+    chip16.write(initial_address + 2, 0x0) #ll
+    chip16.write(initial_address + 3, 0x0) #hh
+
+    chip16.r[0x1] = 0x4
+    chip16.r[0x2] = 0x3
+
+    chip16.step()
+
+    chip16.r[0x1].should.be.eql(0x4 % 0x3)
+
+def test_MOD_rz():
+    #Set RZ to RX MOD RY
+    chip16 = cpu.Cpu()
+
+    initial_address = 0x0000
+    chip16.pc = initial_address
+
+    chip16.write(initial_address, 0xA5) #op code
+    chip16.write(initial_address + 1, 0b00100001) #y,x
+    chip16.write(initial_address + 2, 0x3) #ll
+    chip16.write(initial_address + 3, 0x0) #hh
+
+    chip16.r[0x1] = 0x4
+    chip16.r[0x2] = 0x3
+
+    chip16.step()
+
+    chip16.r[0x3].should.be.eql(0x4 % 0x3)
